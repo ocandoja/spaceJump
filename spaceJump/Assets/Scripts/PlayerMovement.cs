8687ec96b10be0f12d0145c3a479a67674486806
José Ocando <jaogocando@gmail.com>
2021-02-08 12:41:34 -0500

    Update
    Player and mechanics created, first level done, tilemap and physics added.

---
+++ spaceJump/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D playerRb;
+    public float speed = .5f;
+    public float jumpForce = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
